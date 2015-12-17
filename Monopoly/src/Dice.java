8d36a5d357e83326db0599f1e5cb365ad4f51c4a
Jon Cleary <Laptop@10.0.1.56>
2015-12-17 06:53:41 -0700

    Reverse in jail done

--- Monopoly/src/Dice.java
+++ Monopoly/src/Dice.java
@@ -5,6 +5,7 @@ public class Dice
 		static ArrayList<String>ownedProperties = new ArrayList<String>();
 		static Scanner Player=new Scanner(System.in);
 		public static boolean gameContinues=true;
+		
 		public static void rollDice()
 			{
 				int dice1;
@@ -12,24 +13,60 @@ public class Dice
 				int roll;
 				int position = 0;
 				int credits=1500;
+				boolean goBackwards = false;
 				while(0==0) 
 					{
 						System.out.println();
 						System.out.println("Your bank balance is " + credits + ".");
 						System.out.println("Hit Enter to Roll Dice");
+						System.out.println();
 						if(Player.hasNextLine()) 
 							{
 								Player.nextLine();
 								dice1 = (int)(Math.random()*6 + 1);
 								dice2 = (int)(Math.random()*6 + 1);
-								roll = dice1 + dice2;
-								if((position + roll) >= 40) {
-									credits += 200;
-									System.out.println("You received $200 for passing/landing on go");
-									System.out.println("Your balance is " + credits + " credits.");
+								roll = 5;
+								if((position + roll)>=40)
+									{
+										if(goBackwards==false) 
+											{
+												credits+=200;
+												System.out.println("You received $200 for passing Go");
+												System.out.println("Your balance is $" + credits);
+											}
+									}
+								if(goBackwards == false) 
+									{
+										position = (position+roll)%40;
+										System.out.println("You rolled " + roll + ". You landed on " + Board.playBoard.get(position).getName());
+										System.out.println();
+										if(position==10) 
+											{
+												goBackwards=true;
+												System.out.println("Unlucky roll! You will be playing backwards now!");
+												System.out.println();
+											}
+									} 
+								else 
+									{
+										if((position - roll) < 0) 
+											{
+												position =position-roll+40;
+												System.out.println("You rolled " + roll + ". You landed on " + Board.playBoard.get(position).getName());
+												System.out.println();
+											}
+										else
+											{
+												position=position-roll;
+												System.out.println("You rolled " + roll + ". You landed on " + Board.playBoard.get(position).getName());
+												System.out.println();
+											}
+										if(position==10) 
+											{
+												goBackwards=false;
+											}
+
 								}
-								position = (position + roll)%40;
-								System.out.println("You rolled " + roll + ". You landed on " + Board.playBoard.get(position).getName());
 								System.out.println();
 								if(Board.playBoard.get(position).isBuyable() == true)
 									{
@@ -86,10 +123,11 @@ public class Dice
 													{
 														System.out.println("You do not have enough money to pay this fee. Find money immediately.");
 													}
+													
 											}
 									}
 							}
-								
+						 	
 							
 					}
 			}
